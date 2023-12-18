3d78fae053e8baaede75f29f9f58ca6aa19dec0d
Jethro Wong <weijiajethro.wong@stengg.com>
2023-12-18 14:40:36 +0800

    add CSS

--- app/Main.js
+++ app/Main.js
@@ -25,3 +25,144 @@ import StateContext from "./StateContext"
 
 // immerReducer enables state to be accessed throughout app
 // initial state is empty
+
+const initialState = {
+  // for general use
+  loading: true,
+  user: "",
+  overlay: false,
+  toasts: [],
+  toasttype: false,
+  error: "",
+  // for nested use
+  admin: false
+}
+
+function reducer(draft, action) {
+  switch (action.type) {
+    // user session -----------------------------
+    case "update":
+      draft.user = action.user
+      draft.loading = false
+      return
+    case "login":
+      draft.user = action.user
+      draft.toasts.push(action.message)
+      draft.toasttype = true
+      draft.loading = false
+      return
+    case "logout":
+      draft.user = ""
+      draft.overlay = false
+      draft.toasts.push(action.message)
+      draft.toasttype = true
+      draft.loading = false
+      return
+    // toasts --------------------------------
+    case "gtoast":
+      draft.toasts.push(action.message)
+      draft.toasttype = true
+      return
+    case "btoast":
+      draft.toasts.push(action.message)
+      draft.toasttype = false
+      return
+    // profile popup -------------------------
+    case "profile":
+      draft.overlay = !draft.overlay
+      return
+    case "closeprofile":
+      draft.overlay = false
+      return
+    // SIDEBAR admin check
+    case "admin":
+      draft.admin = action.admin
+      return
+    // Server error
+    case "error":
+      draft.error = action.error
+      return
+  }
+}
+
+function MainComponent() {
+  const [state, dispatch] = useImmerReducer(reducer, initialState)
+
+  console.log("main state ", state)
+
+  // check authentication and authorization on page load for display of buttons
+  const fetchAuth = async () => {
+    try {
+      // check permissions: in this case only admin
+      // Make authorization request to the server
+      const token = Cookies.get("token")
+      const response = await Axios.post("/checkgroup", { groupname: "admin", token })
+
+      // if not logged in
+      if (response.data.unauth === "login") {
+        dispatch({
+          type: "logout",
+          message: "Logged out"
+        })
+      } else
+        dispatch({
+          type: "update",
+          user: response.data.user
+        })
+
+      if (response.data.unauth === "role" && state.admin) {
+        dispatch({
+          type: "admin",
+          admin: false
+        })
+      } else dispatch({ type: "admin", admin: true })
+      if (response.data.error) {
+        dispatch({
+          type: "error",
+          error: response.data.error
+        })
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error)
+      // Handle errors as needed
+      dispatch({
+        type: "error",
+        error: "server"
+      })
+    }
+  }
+
+  useEffect(() => {
+    // Call the fetch function when the app first mounts
+    fetchAuth()
+  }, [])
+
+  return (
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <BrowserRouter>
+          <Toast messages={state.toasts} />
+          <Header />
+          {state.overlay && <Profile />}
+          {/* main body */}
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            {/* <Route path="/apps" element={state.user ? <Home onLoad={fetchAuth} /> : state.loading ? <ErrorPage /> : <Login />} /> */}
+            <Route path="/" element={<Navigate to="/apps" replace />} />
+            <Route path="/usermgmt" element={state.user ? <UserMgmt onLoad={fetchAuth} /> : state.loading ? <ErrorPage /> : <Login />} />
+            {/* <Route path="/apps/:appid" element={state.user ? <TaskDashboard onLoad={fetchAuth} /> : state.loading ? <ErrorPage /> : <Login />} />
+            <Route path="/apps/:appid/task/:taskid" element={state.user ? <TaskDashboard onLoad={fetchAuth} /> : state.loading ? <ErrorPage /> : <Login />} /> */}
+            <Route path="/logout" element={<Navigate to="/login" replace />} />
+            <Route path="/error" element={<ErrorPage />} />
+            <Route path="*" element={<Navigate replace to="/error" />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
+  )
+}
+
+//To render page
+const root = ReactDOM.createRoot(document.querySelector("#app"))
+root.render(<MainComponent />)
